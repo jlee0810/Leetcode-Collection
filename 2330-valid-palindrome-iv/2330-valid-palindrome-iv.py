6503261ff2ec72240f5a725421c7a3edca14e88f
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-08-05 21:08:16 +0900

    Time: 52 ms (72.97%), Space: 17 MB (76.64%) - LeetHub

---
+++ 2330-valid-palindrome-iv/2330-valid-palindrome-iv.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        count = 0
+        while l <= r:
+            if s[l] != s[r]:
+                count += 1
+            l += 1
+            r -= 1
+        return count <= 2
