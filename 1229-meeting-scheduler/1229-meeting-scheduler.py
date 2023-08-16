79560cc9279d969cbe44e1f809a4e71cd9fe55e4
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-08-16 10:23:45 +0900

    Time: 544 ms (60.77%), Space: 24.8 MB (63.64%) - LeetHub

---
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        slots1.sort(key=lambda interval: interval[0])
+        slots2.sort(key=lambda interval: interval[0])
+
+        i, j = 0, 0
+
+        while i < len(slots1) and j < len(slots2):
+            working_start = max(slots1[i][0], slots2[j][0])
+            working_end = min(slots1[i][1], slots2[j][1])
+
+            print(working_start, working_end)
+
+            if working_end - working_start >= duration:
+                return [working_start, working_start + duration]
+            
+            if slots1[i][1] > slots2[j][1]:
+                j += 1
+            else:
+                i += 1
+        return []
