17e4ba984b5fb86fa1946e0e849065f25da6beea
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-15 11:03:08 +0900

    Time: 36 ms (50.05%), Space: 16.6 MB (40.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,20 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not len(digits):
-            return []
-
-        map = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
+        charNum = {'2': ['a', 'b', 'c'], '3' : ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q','r', 's'], '8': ['t', 'u', 'v'], '9': ['w','x', 'y','z']}
 
         result = []
 
-        def backtrack(start, combo):
-            if len(combo) == len(digits):
+        if not digits:
+            return ""
+
+        def backtrack(idx, combo):
+            if idx == len(digits):
                 result.append(combo)
                 return
-            for i in range(len(map[digits[start]])):
-                combo += map[digits[start]][i]
-                backtrack(start + 1, combo)
-                combo = combo[:len(combo) - 1]
-
+            for c in charNum[digits[idx]]:
+                combo += c
+                backtrack(idx + 1, combo)
+                combo = combo[:-1]
         backtrack(0, '')
         return result
