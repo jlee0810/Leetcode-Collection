7962b419ee60848d604925c02981291da5e818b1
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-15 09:52:03 +0900

    Time: 40 ms (34.61%), Space: 16.7 MB (61.17%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,13 +2,16 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
 
-        def dfs(start, subset):
-            if start >= len(nums):
+        def backtrack(idx, subset):
+            nonlocal result
+            if idx == len(nums):
                 result.append(subset.copy())
                 return
-            subset.append(nums[start])
-            dfs(start + 1, subset)
+            subset.append(nums[idx])
+            backtrack(idx + 1, subset)
             subset.pop()
-            dfs(start + 1, subset)
-        dfs(0, [])   
+            backtrack(idx + 1, subset)
+        
+        backtrack(0, [])
+
         return result
