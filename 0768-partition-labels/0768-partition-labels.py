9aa43f63455796b84776a3fcfda13153a23680d1
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-17 10:14:21 +0900

    Time: 36 ms (84.98%), Space: 16.5 MB (68.93%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        idx_dic = {}
+        for i in range(len(s)):
+            idx_dic[s[i]] = i
+    
+        result = []
+        curr_len = 0
+        curr_part_max = 0
+        
+        for i in range(len(s)):
+            char = s[i]
+            curr_part_max = max(curr_part_max, idx_dic[char])
+            curr_len += 1
+
+            if i == curr_part_max:
+                result.append(curr_len)
+                curr_len = 0
+        return result
