784e6d6701de8f17cd218f82452f3f7d7d528297
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-19 21:07:04 +0900

    Time: 82 ms (92.86%), Space: 18.2 MB (78.23%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        result = []
+        pre_req_count = [0 for _ in range(numCourses)]
+        adj_list = defaultdict(list)
+
+        for course, prereq in prerequisites:
+            adj_list[prereq].append(course)
+            pre_req_count[course] += 1
+
+        q = deque()
+
+        for idx, count in enumerate(pre_req_count):
+            if count == 0:
+                q.append(idx)
+        
+        while q:
+            course = q.popleft()
+            result.append(course)
+
+            for dependent in adj_list[course]:
+                pre_req_count[dependent] -= 1
+                if pre_req_count[dependent] == 0:
+                    q.append(dependent)
+
+        return result if sum(pre_req_count) == 0 else []
