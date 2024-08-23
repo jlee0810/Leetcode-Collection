b1969af1bcff1e8f65ee307c5124bcfd86b6c992
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-23 15:45:30 +0900

    Time: 213 ms (46.79%), Space: 49.4 MB (80.13%) - LeetHub

--- 1992-sort-linked-list-already-sorted-using-absolute-values/1992-sort-linked-list-already-sorted-using-absolute-values.py
+++ 1992-sort-linked-list-already-sorted-using-absolute-values/1992-sort-linked-list-already-sorted-using-absolute-values.py
@@ -1,26 +1,18 @@
 class Solution:
     def sortLinkedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        
-        list_val = []
-        node_dict = defaultdict(set)
-
         curr = head
+        prev = None
+
         while curr:
-            list_val.append(curr.val)
-            node_dict[curr.val].add(curr)
-            curr = curr.next
-        
-        list_val.sort()
+            if curr.val < 0 and prev is not None:
+                node = curr
+                prev.next = curr.next
+                curr = curr.next
+                
+                node.next = head
+                head = node  
+            else:
+                prev = curr
+                curr = curr.next
 
-        dummy = ListNode(0)
-        curr = dummy
-        for num in list_val:
-            node = node_dict[num].pop()
-            curr.next = node
-            curr = curr.next
-        
-        curr.next = None
-
-        return dummy.next
+        return head
