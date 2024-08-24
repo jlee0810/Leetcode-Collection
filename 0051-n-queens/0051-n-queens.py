70ca0a687954e45f337758d97ae4cd7c3ecbe67b
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-24 20:51:42 +0900

    Time: 71 ms (33.56%), Space: 17 MB (46.62%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,14 +1,14 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        cols = [-1 for _ in range(n)]
         results = []
-
+        cols = [-1 for _ in range(n)]
+        
         def can_place(row, col):
             for i in range(row):
-                if cols[i] == col or abs(i - row) == abs(col - cols[i]):
+                if cols[i] == col or abs(i - row) == abs(cols[i] - col):
                     return False
             return True
-
+        
         def backtrack(row):
             if row == n:
                 results.append(cols.copy())
@@ -18,9 +18,11 @@ class Solution:
                     cols[row] = col
                     backtrack(row + 1)
                     cols[row] = -1
+        
         backtrack(0)
 
         for result in results:
             for i in range(n):
-                result[i] = '.' * result[i] + 'Q' + '.' * (n - result[i] - 1)
+                result[i] = "." * result[i] + 'Q' + '.' * (n - result[i] - 1)
+        
         return results
