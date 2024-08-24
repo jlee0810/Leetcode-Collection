cfe45d6f8c168dce42ce4ae84f87c577960b1a67
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-24 22:27:37 +0900

    Time: 730 ms (30.25%), Space: 64.7 MB (65.23%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,13 +2,13 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
 
-        def dfs(start, combination):
-            if len(combination) == k:
-                result.append(combination.copy())
+        def backtrack(start, combo):
+            if len(combo) == k:
+                result.append(combo.copy())
                 return
             for i in range(start, n + 1):
-                combination.append(i)
-                dfs(i + 1, combination)
-                combination.pop()
-        dfs(1, [])
+                combo.append(i)
+                backtrack(i + 1, combo)
+                combo.pop()
+        backtrack(1, [])
         return result
