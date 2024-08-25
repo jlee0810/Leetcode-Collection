588ef2320eb76f5ebbeaa8a4d6552812ad9971f9
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-25 12:14:50 +0900

    Time: 38 ms (73.61%), Space: 16.8 MB (6.23%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
+        visited = [False for _ in range(len(nums))]
 
-        def backtrack(remain, permutation):
-            if len(remain) == 0:
-                result.append(permutation.copy())
+        def backtrack(permute):
+            if len(permute) == len(nums):
+                result.append(permute.copy())
                 return
-            
-            for i in range(len(remain)):
-                permutation.append(remain[i])
-                newNum = remain[:i] + remain[i + 1 : ]
-                backtrack(newNum, permutation)
-                permutation.pop()
-        backtrack(nums, [])
+            for i in range(len(visited)):
+                if visited[i]:
+                    continue
+                visited[i] = True
+                permute.append(nums[i])
+                backtrack(permute)
+                visited[i] = False
+                permute.pop()
+
+        backtrack([])
+
         return result
