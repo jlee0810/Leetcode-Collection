50e5ecd7be58750f4861e0b470a43f020ba4a5d7
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-26 22:46:37 +0900

    Time: 46 ms (25.24%), Space: 18.3 MB (85.77%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,33 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        q = deque()
-        q.append((root, float('-inf'), float('inf')))
+        def dfs(node, min_val, max_val):
+            if not node:
+                return True
+            left_valid = dfs(node.left, min_val, node.val)
+            right_valid = dfs(node.right, node.val, max_val)
 
-        while q:
-            curr_node, min_val, max_val = q.popleft()
-            
-            if curr_node.val <= min_val or curr_node.val >= max_val:
-                return False
-            
-            if curr_node.left:
-                q.append((curr_node.left, min_val, curr_node.val))
-            
-            if curr_node.right:
-                q.append((curr_node.right, curr_node.val, max_val))
-        
-        return True
-
-
-        # def dfs(node, min_val, max_val):
-        #     if not node:
-        #         return True
-        #     if min_val >= node.val or max_val <= node.val:
-        #         return False
-        #     left = dfs(node.left, min_val, node.val)
-        #     right = dfs(node.right, node.val, max_val)
-        #     if left and right:
-        #         return True
-        #     else:
-        #         return False
-        # return dfs(root, float(-inf), float(inf))            
+            if left_valid and right_valid and min_val < node.val < max_val:
+                return True
+            return False
+        return dfs(root, float('-inf'), float('inf'))
