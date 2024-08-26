83881363417f569b6fa4330934a0505ff094bb61
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-26 22:42:45 +0900

    Time: 156 ms (9.58%), Space: 31.1 MB (39.13%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -7,16 +7,30 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         count = 0
-        q = deque()
-        q.append((root, root.val))
 
-        while q:
-            curr_node, curr_max = q.popleft()
-            if curr_node.val >= curr_max:
+        def dfs(node, max_val):
+            nonlocal count
+            if not node:
+                return
+            if node.val >= max_val:
                 count += 1
-            if curr_node.left:
-                q.append((curr_node.left, max(curr_max, curr_node.val)))
-            if curr_node.right:
-                q.append((curr_node.right, max(curr_max, curr_node.val)))
-            
+            dfs(node.left, max(max_val, node.val))
+            dfs(node.right, max(max_val, node.val))
+        
+        dfs(root, root.val)
         return count
+
+        # count = 0
+        # q = deque()
+        # q.append((root, root.val))
+        
+        # while q:
+        #     curr_node, curr_max = q.popleft()
+        #     if curr_node.val >= curr_max:
+        #         count += 1
+        #     if curr_node.left:
+        #         q.append((curr_node.left, max(curr_max, curr_node.val)))
+        #     if curr_node.right:
+        #         q.append((curr_node.right, max(curr_max, curr_node.val)))
+            
+        # return count
