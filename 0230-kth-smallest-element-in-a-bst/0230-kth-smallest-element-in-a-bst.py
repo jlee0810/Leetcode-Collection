4177c0b0b6e71a36b8a5d11e5e9a60698e6eb646
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-26 22:52:44 +0900

    Time: 39 ms (87.44%), Space: 19.5 MB (71.07%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,41 +6,16 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        #First Attempt Min Heap
-        # minHeap = []
-
-        # def dfs(node):
-        #     if not node:
-        #         return 
-        #     heappush(minHeap, node.val)
-        #     dfs(node.left)
-        #     dfs(node.right)
-        # dfs(root)
-        
-        # for _ in range(k - 1):
-        #     heappop(minHeap)
-        # return minHeap[0]
-
-
-        #Second Method In Order Traversal
-        def inOrder(root):
+        def inorder(node):
             nonlocal k
-            if root and k > 0:
-                inOrder(root.left)
-
-                # decrement k each time you find a value
-                k -= 1
-                if k == 0:
-                    self.result = root.val
-
-                # continue to recur on the right child
-                inOrder(root.right)
-
-        self.result = None
-        inOrder(root)
-        return self.result
-
-
-
-
-
+            if not node:
+                return None
+            left = inorder(node.left)
+            if left is not None:
+                return left
+            k -= 1
+            if k == 0:
+                return node.val
+            right = inorder(node.right)
+            return right
+        return inorder(root)
