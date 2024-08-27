cae2309a571175df73ae9261c6884fcf33f39c9d
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-27 13:42:26 +0900

    Time: 890 ms (96.34%), Space: 40.4 MB (56.59%) - LeetHub

---
+++ 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findLonely(self, nums: List[int]) -> List[int]:
+        cnt = Counter(nums)
+
+        result = []
+        for key, value in cnt.items():
+            if value == 1:
+                if key - 1 not in cnt and key + 1 not in cnt:
+                    result.append(key)
+        
+        return result
