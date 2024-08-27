54ae84de390adfde6779640a313620d4cdb53a0a
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-27 11:12:55 +0900

    Time: 156 ms (58.48%), Space: 19.3 MB (13.5%) - LeetHub

--- 0839-short-encoding-of-words/0839-short-encoding-of-words.py
+++ 0839-short-encoding-of-words/0839-short-encoding-of-words.py
@@ -1,8 +1,31 @@
+class TrieNode:
+    def __init__(self):
+        self.child = {}
+        self.end = False 
+
 class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> TrieNode:
+        curr = self.root
+        for c in word:
+            if c not in curr.child:
+                curr.child[c] = TrieNode()
+            curr = curr.child[c]
+        curr.end = True
+        return curr
+
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        good = set(words)
+        words = set(words)
+        nodes = {}
         for word in words:
-            for k in range(1, len(word)):
-                good.discard(word[k:])
+            node = self.addWord(word[::-1])
+            nodes[node] = len(word) + 1
+
+        total_length = 0
+        for node, length in nodes.items():
+            if len(node.child) == 0:
+                total_length += length
         
-        return sum(len(word) + 1 for word in good)
+        return total_length
