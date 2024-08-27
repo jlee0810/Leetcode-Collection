309ed9f2564c659706ddbee56134aaa1268c497e
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-28 08:42:15 +0900

    Time: 86 ms (80.47%), Space: 18.2 MB (92.84%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -4,11 +4,10 @@ class Solution:
         parent = [i for i in range(n)]
 
         def find(node):
-            if parent[node] != node:
-                return find(parent[node])
-            else:
-                return node
-        
+            while parent[node] != node:
+                node = parent[node]
+            return node
+
         def union(node1, node2):
             parent1 = find(node1)
             parent2 = find(node2)
@@ -25,9 +24,9 @@ class Solution:
 
         for n1, n2 in edges:
             union(n1, n2)
-        
-        s_parent = set()
-        for i in range(n):
-            s_parent.add(find(i))
 
-        return len(s_parent)
+        parent_set = set()
+        for i in range(n):
+            parent_set.add(find(i))
+        
+        return len(parent_set)
