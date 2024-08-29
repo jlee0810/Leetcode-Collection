483f176370d27e7e2468e5136106790ddd2d0027
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-29 10:07:10 +0900

    Time: 469 ms (54.03%), Space: 17 MB (63.87%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,20 +1,20 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         time = 0
-        cooldown = deque()
         max_heap = []
-        count = Counter(tasks)
+        cooldown = deque()
+        cnt = Counter(tasks)
 
-        for task in count.values():
+        for task in cnt.values():
             heappush(max_heap, -task)
         
-
         while max_heap or cooldown:
             time += 1
             if max_heap:
-                cnt = heappop(max_heap) + 1
-                if cnt < 0:
-                    cooldown.append([cnt, time + n])
+                task_left = heappop(max_heap) + 1
+                if task_left < 0:
+                    cooldown.append([task_left, time + n])
             if cooldown and cooldown[0][1] <= time:
                 heappush(max_heap, cooldown.popleft()[0])
+        
         return time
