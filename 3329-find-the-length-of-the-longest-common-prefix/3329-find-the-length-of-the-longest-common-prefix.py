053de4463a1ee61a99d1967d4a77f14a7f0d19d1
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-30 14:39:50 +0900

    Time: 909 ms (56.67%), Space: 33.5 MB (21.09%) - LeetHub

---
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_end = True
+
+class Solution:
+    def traverse_trie(self, node, curr_word, level):
+        for ch in curr_word:
+            if ch not in node.children:
+                return level
+            level += 1
+            node = node.children[ch]
+        return level
+
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        trie = Trie()
+        for num in arr1:
+            trie.insert(str(num))
+
+        res = 0
+        for num in arr2:
+            level = self.traverse_trie(trie.root, str(num), 0)
+            res = max(res, level)
+
+        return res
