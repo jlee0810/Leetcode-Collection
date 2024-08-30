9c16729da19a8a1e45bd6951d13ebeffe3162d01
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-30 14:16:56 +0900

    Time: 86 ms (77.37%), Space: 18.3 MB (79.95%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        prereq_list = defaultdict(list)
-        in_degree = [0] * numCourses
-        
+        pre_req_list = defaultdict(list)
+        in_degree = [0 for _ in range(numCourses)]
+
         for course, pre_req in prerequisites:
-            prereq_list[pre_req].append(course)
+            pre_req_list[pre_req].append(course)
             in_degree[course] += 1
         
-        q = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        q = deque()
+        for i in range(numCourses):
+            if in_degree[i] == 0:
+                q.append(i)
         
-        processed_courses = 0
-        
+        took = 0
+
         while q:
             course = q.popleft()
-            processed_courses += 1
-            
-            for dependent in prereq_list[course]:
+            took += 1
+            for dependent in pre_req_list[course]:
                 in_degree[dependent] -= 1
                 if in_degree[dependent] == 0:
                     q.append(dependent)
         
-        return processed_courses == numCourses
+        return took == numCourses
