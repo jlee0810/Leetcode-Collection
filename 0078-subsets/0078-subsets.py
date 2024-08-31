609afb06fb9b9fbb4ee7d4efd3708b5038edbb2d
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-01 08:14:39 +0900

    Time: 46 ms (5.79%), Space: 16.7 MB (25.9%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,13 +2,15 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
 
-        def backtrack(idx, sub):
+        def backtrack(idx, ss):
             if idx == len(nums):
-                result.append(sub.copy())
+                result.append(ss.copy())
                 return
-            sub.append(nums[idx])
-            backtrack(idx + 1, sub)
-            sub.pop()
-            backtrack(idx + 1, sub)
+            ss.append(nums[idx])
+            backtrack(idx + 1, ss)
+            ss.pop()
+            backtrack(idx + 1, ss)
+
         backtrack(0, [])
+
         return result
