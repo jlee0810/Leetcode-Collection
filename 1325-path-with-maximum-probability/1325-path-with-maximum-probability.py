34ced0126f80013e873e6bdeecb9f31fbcb6e59e
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-08-31 09:18:28 +0900

    Time: 527 ms (88.35%), Space: 28.1 MB (69.45%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -4,20 +4,21 @@ class Solution:
         for (v1, v2), prob in zip(edges, succProb):
             adj_list[v1].append((v2, prob))
             adj_list[v2].append((v1, prob))
-
+        
         pq = [(-1, start_node)]
-        prob = [0] * n
+        prob = [0 for _ in range(n)]       
         prob[start_node] = 1
 
         while pq:
-            curr_prob, node = heapq.heappop(pq)
+            curr_prob, curr_node = heappop(pq)
             curr_prob *= -1
 
-            if curr_prob < prob[node]:
+            if curr_prob < prob[curr_node]:
                 continue
+            
+            for neighbor, success_prob in adj_list[curr_node]:
+                new_prob = curr_prob * success_prob
 
-            for neighbor, success_prob in adj_list[node]:
-                new_prob = success_prob * curr_prob
                 if new_prob > prob[neighbor]:
                     prob[neighbor] = new_prob
                     heapq.heappush(pq, (-new_prob, neighbor))
