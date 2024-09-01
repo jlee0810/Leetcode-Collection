61032c5611e4800738224b263c793b533b2c4298
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-01 22:12:53 +0900

    Time: 210 ms (77.23%), Space: 21.6 MB (19.85%) - LeetHub

--- 1190-smallest-common-region/1190-smallest-common-region.py
+++ 1190-smallest-common-region/1190-smallest-common-region.py
@@ -1,20 +1,24 @@
 class Solution:
     def findSmallestRegion(self, regions: List[List[str]], region1: str, region2: str) -> str:
         parent = {}
-        
         for region in regions:
             for child in region[1:]:
                 parent[child] = region[0]
 
-        def dfs(region, ancestors):
-            if region in parent:
-                dfs(parent[region], ancestors)
-            ancestors.add(region)
+        def path_to_root(region: str) -> Set[str]:
+            path = []
+            while region in parent:
+                path.append(region)
+                region = parent[region]
+            path.append(region)
+            return path
         
-        ancestors1 = set()
-        dfs(region1, ancestors1)
+        path1 = path_to_root(region1)
+        path2 = path_to_root(region2)
 
-        while region2 not in ancestors1:
-            region2 = parent[region2]
-        
-        return region2
+        set_path1 = set(path1)
+        for region in path2:
+            if region in set_path1:
+                return region
+    
+        return -1
