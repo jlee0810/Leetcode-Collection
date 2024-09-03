7d9bd630068648695555a07629f12427a209b94e
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-03 20:29:37 +0900

    Time: 1199 ms (42.6%), Space: 62.7 MB (9.47%) - LeetHub

--- 2367-minimum-lines-to-represent-a-line-chart/2367-minimum-lines-to-represent-a-line-chart.py
+++ 2367-minimum-lines-to-represent-a-line-chart/2367-minimum-lines-to-represent-a-line-chart.py
@@ -1,28 +1,24 @@
 class Solution:
     def minimumLines(self, stockPrices: List[List[int]]) -> int:
         stockPrices.sort()
-        
-        def normalize(dx, dy):
-            if dx == 0:
-                return (1, 0) if dy > 0 else (-1, 0)
-            if dy == 0:
-                return (0, 1) if dx > 0 else (0, -1)
+
+        def slope(x1, y1, x2, y2):
+            dx, dy = x2 - x1, y2- y1
             g = gcd(dx, dy)
             return (dy // g, dx // g) if dx > 0 else (-dy // g, -dx // g)
-        
+
         lines = 0
-        last_slope = None
-        
+
+        last_slope = float('inf')
         for i in range(1, len(stockPrices)):
             x1, y1 = stockPrices[i - 1]
             x2, y2 = stockPrices[i]
 
-            dx = x2 - x1
-            dy = y2 - y1
-            
-            curr_slope = normalize(dx, dy)
-            
-            if curr_slope != last_slope:
+            curr_slope = slope(x1, y1, x2, y2)
+
+            if curr_slope == last_slope:
+                continue
+            else:
                 lines += 1
                 last_slope = curr_slope
         
