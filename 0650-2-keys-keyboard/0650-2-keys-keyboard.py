8626a225946cdee73f2ca539256bddb98180e0ed
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-03 09:49:15 +0900

    Time: 286 ms (16.53%), Space: 126.9 MB (5.43%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -2,18 +2,17 @@ class Solution:
     def minSteps(self, n: int) -> int:
         result = float('inf')
 
-        @lru_cache
+        @lru_cache(None)
         def backtrack(curr_len, curr_memory, curr_count, last_move_copy):
             nonlocal result
+
             if curr_len == n:
                 result = min(result, curr_count)
                 return
             if curr_len > n:
                 return
-            
             if not last_move_copy:
                 backtrack(curr_len, curr_len, curr_count + 1, True)
-            
             if curr_memory > 0:
                 backtrack(curr_len + curr_memory, curr_memory, curr_count + 1, False)
         
