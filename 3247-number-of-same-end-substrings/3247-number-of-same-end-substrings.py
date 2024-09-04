1f75ddd3f0138ca716f0b7b360b74c08f10518b9
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-04 11:19:33 +0900

    Time: 2713 ms (35.34%), Space: 53.6 MB (19.4%) - LeetHub

---
+++ 3247-number-of-same-end-substrings/3247-number-of-same-end-substrings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sameEndSubstringCount(self, s: str, queries: List[List[int]]) -> List[int]:
+        char_counter = Counter()
+        prefix_counters = [Counter()]
+
+        for char in s:
+            char_counter[char] += 1
+            prefix_counters.append(char_counter.copy())
+
+        def calculate_contribution(counter):
+            return sum(count * (count + 1) // 2 for count in counter.values())
+
+        results = []
+        
+        for left, right in queries:
+            right_counter = prefix_counters[right + 1]
+            left_counter = prefix_counters[left]
+
+            substring_counter = right_counter - left_counter
+            contribution = calculate_contribution(substring_counter)
+            results.append(contribution)
+        
+        return results
