007b3f31509658b3b46b7128aa788858c7867e41
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-05 11:40:55 +0900

    Time: 1004 ms (42.24%), Space: 26.8 MB (57.22%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.py
+++ 2155-find-missing-observations/2155-find-missing-observations.py
@@ -14,7 +14,7 @@ class Solution:
         
         for i in range(remainder):
             result[i] += 1
-        
+
         for r in result:
             if r < 1 or r > 6:
                 return []
