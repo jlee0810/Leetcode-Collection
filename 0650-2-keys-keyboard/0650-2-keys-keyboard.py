9e4970c23e2f21f37e23b154a4e41500913a1413
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-06 08:30:27 +0900

    Time: 325 ms (15.7%), Space: 16.6 MB (58.53%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,7 +1,7 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         result = float('inf')
-
+        
         def backtrack(curr_len, curr_memory, last_copy, count):
             nonlocal result
             if curr_len == n:
@@ -13,6 +13,7 @@ class Solution:
                 backtrack(curr_len, curr_len, True, count + 1)
             if curr_memory > 0:
                 backtrack(curr_len + curr_memory, curr_memory, False, count + 1)
+
         backtrack(1, 0, False, 0)
 
         return result
