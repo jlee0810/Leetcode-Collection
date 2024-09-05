fe2099378089bb241461058bb88434b31908ce76
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-06 08:42:05 +0900

    Time: 60 ms (98.57%), Space: 20.8 MB (6.97%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,19 +1,5 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # intervals.sort()
-        # rooms = 0
-        # min_heap = []
-
-        # for interval in intervals:
-        #     if min_heap and min_heap[0] <= interval[0]:
-        #         heappop(min_heap)
-        #         heappush(min_heap, interval[1])
-        #     else:
-        #         heappush(min_heap, interval[1])
-        #         room = max(rooms, len(min_heap))
-        
-        # return room
-        
         time = defaultdict(int)
 
         for startTime, endTime in intervals:
