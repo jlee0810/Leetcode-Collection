ff8c478483d950fba2de18ab525598272e623eb8
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-08 10:08:45 +0900

    Time: 264 ms (70%), Space: 17.8 MB (36.36%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,22 +1,23 @@
+from sortedcontainers import SortedList
+
 class MyCalendarTwo:
     def __init__(self):
-        self.times = defaultdict(int)
-        
-    def book(self, startTime: int, endTime: int) -> int:
-        self.times[startTime] += 1
-        self.times[endTime] -= 1
+        self.calendar = SortedList()
+        self.overlaps = SortedList()
 
-        intersect = 0
-
-        for t in sorted(self.times):
-            intersect += self.times[t]
-            if intersect >= 3:
-                self.times[startTime] -= 1
-                self.times[endTime] += 1
+    def book(self, start: int, end: int) -> bool:
+        for o_start, o_end in self.overlaps:
+            if start < o_end and end > o_start:
                 return False
+
+        for c_start, c_end in self.calendar:
+            if start < c_end and end > c_start:
+                overlap_start = max(start, c_start)
+                overlap_end = min(end, c_end)
+                self.overlaps.add((overlap_start, overlap_end))
+        
+        self.calendar.add((start, end))
         return True
-
-
 
 # Your MyCalendarTwo object will be instantiated and called as such:
 # obj = MyCalendarTwo()
