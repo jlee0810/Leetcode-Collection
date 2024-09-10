97f408a0bd782c7c2701bdb9009805e76ef091e1
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-10 10:13:57 +0900

    Time: 119 ms (27.53%), Space: 17.8 MB (87.45%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.py
+++ 0690-employee-importance/0690-employee-importance.py
@@ -14,20 +14,33 @@ class Solution:
         for employee in employees:
             employee_dict[employee.id] = employee
 
+        # DFS
+        def dfs(id):
+            employee_obj = employee_dict[id]
+            if not employee_obj.subordinates:
+                return employee_obj.importance
+            subord_import_total = 0
+            for subord_id in employee_obj.subordinates:
+                subord_import_total += dfs(subord_id)
+            
+            return employee_dict[id].importance + subord_import_total
+        
+        return dfs(id)
 
-        for employee in employees:
-            if employee.id != id:
-                continue
-            if employee.id == id:
-                q = deque()
-                q.append(employee)
-                total_importance = employee.importance
+        # BFS
+        # for employee in employees:
+        #     if employee.id != id:
+        #         continue
+        #     if employee.id == id:
+                # q = deque()
+                # q.append(employee)
+                # total_importance = employee.importance
 
-                while q:
-                    curr_employee = q.popleft()
-                    for subord in curr_employee.subordinates:
-                        subord_obj = employee_dict[subord]
-                        total_importance += subord_obj.importance
-                        q.append(subord_obj)
+                # while q:
+                #     curr_employee = q.popleft()
+                #     for subord in curr_employee.subordinates:
+                #         subord_obj = employee_dict[subord]
+                #         total_importance += subord_obj.importance
+                #         q.append(subord_obj)
                 
-                return total_importance
+                # return total_importance
