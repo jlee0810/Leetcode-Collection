b84d3b9321c1c8fbf2e82380332a9c6e565ed412
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-11 08:27:40 +0900

    Time: 209 ms (76.14%), Space: 16.7 MB (85.79%) - LeetHub

--- 0843-binary-trees-with-factors/0843-binary-trees-with-factors.py
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.py
@@ -3,14 +3,14 @@ class Solution:
         mod = 10 ** 9 + 7
         n = len(arr)
         arr.sort()
-        dp = [1] * n
+        dp = [1 for _ in range(n)]
 
-        index = {x: i for i, x in enumerate(arr)}
-        
+        index = {x : i for i, x in enumerate(arr)}
+
         for i, x in enumerate(arr):
             for j in range(i):
                 if x % arr[j] == 0:
-                    right = x / arr[j]
+                    right = x // arr[j]
                     if right in index:
                         dp[i] += dp[j] * dp[index[right]]
                         dp[i] %= mod
