a7953362877d8a087891c3d483b32edf773cdc6c
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-21 09:25:17 +0900

    Time: 64 ms (91.99%), Space: 22.6 MB (66.63%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        result = []
+        current = 1
+
+        for _ in range(n):
+            result.append(current)
+
+            if current * 10 <= n:
+                current *= 10
+            else:
+                while current % 10 == 9 or current >= n:
+                    current //= 10
+                current += 1
+
+        return result
