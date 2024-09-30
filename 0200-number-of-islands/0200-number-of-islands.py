e5388bf765f4b286c5b134e114944e2e7d3ae96c
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-09-29 20:45:19 -0700

    Time: 240 ms (64.84%), Space: 18.8 MB (96.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,21 +3,21 @@ class Solution:
         def bfs(r, c):
             q = deque()
             q.append((r, c))
-            
+
             while q:
                 curr_r, curr_c = q.popleft()
-                for dr, dc in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-                    nr, nc = curr_r + dr, curr_c + dc
+                grid[curr_r][curr_c] = "0"
 
+                for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    nr, nc = curr_r + dr, curr_c + dc
                     if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == "1":
                         grid[nr][nc] = "0"
                         q.append((nr, nc))
-
-        count = 0
+        num_island = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
+                if grid[i][j] == "1":
                     bfs(i, j)
-                    count += 1
-                    
-        return count
+                    num_island += 1
+                
+        return num_island
