ebd57c27d016a9b9f332102df72b7805f9494667
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-10-07 16:47:49 -0700

    Time: 953 ms (15.68%), Space: 31.3 MB (50.37%) - LeetHub

--- 2419-subarray-with-elements-greater-than-varying-threshold/2419-subarray-with-elements-greater-than-varying-threshold.py
+++ 2419-subarray-with-elements-greater-than-varying-threshold/2419-subarray-with-elements-greater-than-varying-threshold.py
@@ -1,30 +1,31 @@
 class Solution:
     def validSubarraySize(self, nums: List[int], threshold: int) -> int:
-        n = len(nums)
-        left = [-1] * n
-        right = [n] * n
-        stack = []
-        
-        for i in range(n):
-            while stack and nums[stack[-1]] >= nums[i]:
-                stack.pop()
-            if stack:
-                left[i] = stack[-1]
-            stack.append(i)
+        left = [-1 for _ in range(len(nums))]
+        right = [len(nums) for _ in range(len(nums))]
 
         stack = []
-        
-        for i in range(n - 1, -1, -1):
-            while stack and nums[stack[-1]] >= nums[i]:
+
+        for idx in range(len(nums)):
+            while stack and nums[stack[-1]] >= nums[idx]:
                 stack.pop()
             if stack:
-                right[i] = stack[-1]
-            stack.append(i)
+                left[idx] = stack[-1]
+            stack.append(idx)
 
-            
-        for i in range(n):
+        stack = []
+
+        for idx in range(len(nums) - 1, -1, -1):
+            while stack and nums[stack[-1]] >= nums[idx]:
+                stack.pop()
+            if stack:
+                right[idx] = stack[-1]
+            stack.append(idx)
+        
+        for i in range(len(nums)):
             k = right[i] - left[i] - 1
             if nums[i] > threshold / k:
                 return k
-        
+
+ 
         return -1
+        
