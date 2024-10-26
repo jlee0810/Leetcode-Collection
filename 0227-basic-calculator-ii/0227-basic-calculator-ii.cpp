760fd3f5ecb4b64600f7f3cb7bab80f916b6b244
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-10-26 12:04:24 -0700

    Time: 10 ms (89.57%), Space: 14 MB (54.42%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> stck;
+        char prev_op = '+';
+        int num = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            char c = s[i];
+
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+
+            if (!isdigit(c) && c != ' ' || i == s.size() - 1) {
+                if (prev_op == '+') {
+                    stck.push(num);
+                } else if (prev_op == '-') {
+                    stck.push(-num);
+                } else if (prev_op == '*') {
+                    int top = stck.top();
+                    stck.pop();
+                    stck.push(top * num);
+                } else if (prev_op == '/') {
+                    int top = stck.top();
+                    stck.pop();
+                    stck.push(top / num);
+                }
+
+                prev_op = c;
+                num = 0;
+            }
+        }
+
+        int result = 0;
+        while (!stck.empty()) {
+            result += stck.top();
+            stck.pop();
+        }
+
+        return result;
+    }
+};
