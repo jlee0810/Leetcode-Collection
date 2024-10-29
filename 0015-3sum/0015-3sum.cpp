cc06abfff1046fa2dfd29e468c66e78c650af555
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-10-28 22:34:29 -0700

    Time: 48 ms (72.22%), Space: 27.6 MB (51.03%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        
+        int start = 0;
+
+        while (start < nums.size() - 2) {
+            int l = start + 1;
+            int r = nums.size() - 1;
+            
+            while (l < r) {
+                int curr_sum = nums[start] + nums[l] + nums[r];
+                if (curr_sum == 0) {
+                    result.push_back({nums[start], nums[l], nums[r]});
+                    while (l < nums.size() - 1) {
+                        if (nums[l + 1] == nums[l]) {
+                            l++;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    l++; 
+
+                    while (r > start + 1) {
+                        if (nums[r - 1] == nums[r]) {
+                            r -= 1;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    r--;
+                }
+                else if (curr_sum < 0) {
+                    l++;
+                }
+                else {
+                    r--;
+                }
+            }
+            
+            while (start < nums.size() - 2) {
+                if (nums[start] == nums[start + 1]) {
+                    start++;
+                }
+                else {
+                    break;
+                }
+            }
+            start++;
+        }
+
+        return result;
+    }
+};
