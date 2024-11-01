d427fef521e835598281a19eb2b518997af1eaa9
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-11-01 08:26:47 -0700

    Time: 172 ms (15.04%), Space: 43.4 MB (6.67%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
@@ -1,38 +1,31 @@
 class TrieNode:
     def __init__(self, val = None):
         self.val = val
-        self.isfolder = False
-        self.childfolder = {}
+        self.child = {}
+        self.end = False
 
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         root = TrieNode()
-        for f in folder:
-            parse = f.split('/')
-            parse = parse[1:]
-            curr = root
 
-            for name in parse:
-                if name not in curr.childfolder:
-                    curr.childfolder[name] = TrieNode(name)
-                    curr = curr.childfolder[name]
-                else:
-                    curr = curr.childfolder[name]
-            curr.isfolder = True
-        
+        for f in folder:
+            curr = root
+            for name in f.split("/")[1:]:
+                if name not in curr.child:
+                    curr.child[name] = TrieNode(name)
+                curr = curr.child[name]
+            curr.end = True
+
         result = []
         for f in folder:
-            parse = f.split('/')
-            parse = parse[1: - 1]
+            isSubFolder = False
             curr = root
-
-            issubfolder = False
-            for name in parse:
-                curr = curr.childfolder[name]
-                if curr.isfolder:
-                    issubfolder = True
+            for name in f.split('/')[1:-1]:
+                curr = curr.child[name]
+                if curr.end:
+                    isSubFolder = True
                     break
-            if (not issubfolder):
+            if not isSubFolder:
                 result.append(f)
-        
+
         return result
