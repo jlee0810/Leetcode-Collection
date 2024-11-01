3e54cb7b013a8f28a3290fe7401433a5028661cc
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-11-01 08:47:58 -0700

    Time: 63 ms (12.35%), Space: 20.3 MB (89.41%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -10,14 +10,12 @@ class Solution:
         def dfs(node):
             if not node:
                 return
-
             exists_left = dfs(node.left)
             exists_right = dfs(node.right)
 
-            if exists_left and exists_right or node == p or node == q:
+            if (exists_left and exists_right) or node == p or node == q:
                 return node
             else:
                 return exists_left or exists_right
-                
+        
         return dfs(root)
-            
