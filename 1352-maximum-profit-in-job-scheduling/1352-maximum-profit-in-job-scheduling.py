7b2c3ccc04894d8f345fd3f146018790035c48c5
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-10-31 23:34:14 -0700

    Time: 369 ms (14.72%), Space: 57.4 MB (17.09%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -1,19 +1,33 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        intervals = [[start, end, profit] for start, end, profit in zip(startTime, endTime, profit)]
-        intervals.sort()
-
         cache = {}
 
+        intervals = sorted(zip(startTime, endTime, profit), key=lambda x: x[0])
+
+        def b_search(curr_end):
+            result = len(intervals)
+            start, end = 0, len(intervals) - 1
+            while start <= end:
+                mid = (start + end) // 2
+                if intervals[mid][0] >= curr_end:
+                    result = mid
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            return result
+
         def dp(i):
-            if i == len(startTime):
+            if i == len(intervals):
                 return 0
             if i in cache:
                 return cache[i]
-            result = dp(i + 1)
-            next_interval = bisect.bisect_right(intervals, [intervals[i][1], 0, 0])
-            result = max(result, dp(next_interval) + intervals[i][2])
-            cache[i] = result
-            return result
-
+            
+            bestProfit = dp(i + 1)
+            
+            next_avail_idx = b_search(intervals[i][1])
+            bestProfit = max(bestProfit, dp(next_avail_idx) + intervals[i][2])
+            
+            cache[i] = bestProfit
+            return cache[i]
+        
         return dp(0)
