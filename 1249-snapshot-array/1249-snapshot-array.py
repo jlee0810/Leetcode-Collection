10c622054ce985d29f580edab412457b4e6c615b
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-10-31 22:43:02 -0700

    Time: 246 ms (31.73%), Space: 47.7 MB (23.32%) - LeetHub

--- 1249-snapshot-array/1249-snapshot-array.py
+++ 1249-snapshot-array/1249-snapshot-array.py
@@ -2,18 +2,19 @@ class SnapshotArray:
 
     def __init__(self, length: int):
         self.snapshot = [[[0, 0]] for _ in range(length)]
-        self.snapid = 0
-        
+        self.snapId = 0
+
     def set(self, index: int, val: int) -> None:
-        self.snapshot[index].append([self.snapid, val])
+        self.snapshot[index].append([self.snapId, val])
 
     def snap(self) -> int:
-        self.snapid += 1
-        return self.snapid - 1
-
+        self.snapId += 1
+        return self.snapId - 1
+        
     def get(self, index: int, snap_id: int) -> int:
-        snap_index = bisect.bisect_right(self.snapshot[index], [snap_id, float('inf')])
-        return self.snapshot[index][snap_index - 1][1]
+        snapIdx = bisect.bisect_left(self.snapshot[index], [snap_id, float('inf')])
+        print(snapIdx)
+        return self.snapshot[index][snapIdx - 1][1]
 
 
 # Your SnapshotArray object will be instantiated and called as such:
