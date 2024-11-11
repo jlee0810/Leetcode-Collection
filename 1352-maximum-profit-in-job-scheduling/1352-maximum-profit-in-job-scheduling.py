521938bb2bbc685c0dc0c453c8fc625dc3d66547
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-11-11 11:59:58 -0800

    Time: 283 ms (33.4%), Space: 53.6 MB (28.79%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -2,20 +2,22 @@ class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         cache = {}
 
-        intervals = [[start, end, p] for start, end, p in zip(startTime, endTime, profit)]
+        intervals = []
+        for start, end, p in zip(startTime, endTime, profit):
+            intervals.append([start, end, p])
         intervals.sort()
-        
+        print(intervals)
+
         def dp(i):
             if i == len(startTime):
                 return 0
             if i in cache:
                 return cache[i]
-            
-            best_profit = dp(i + 1) #skip i
+            most_profit = dp(i + 1)
             next_avail_idx = bisect.bisect_left(intervals, [intervals[i][1], 0, 0])
-            best_profit = max(best_profit, dp(next_avail_idx) + intervals[i][2])
+            most_profit = max(most_profit, dp(next_avail_idx) + intervals[i][2])
 
-            cache[i] = best_profit
-            return best_profit 
+            cache[i] = most_profit
+            return most_profit
 
         return dp(0)
