c4aeb1f9d736f03dc3c13329bb15a6268f9c898f
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-11-16 12:36:21 -0800

    Time: 58 ms (17.59%), Space: 17.8 MB (60.31%) - LeetHub

--- 1209-design-bounded-blocking-queue/1209-design-bounded-blocking-queue.py
+++ 1209-design-bounded-blocking-queue/1209-design-bounded-blocking-queue.py
@@ -1,15 +1,28 @@
-from queue import Queue
+from collections import deque
+import threading
 
 class BoundedBlockingQueue(object):
-
     def __init__(self, capacity: int):
-        self.q = Queue(maxsize=capacity)
+        self.cond = threading.Condition()
+        self.q = deque()
+        self.capacity = capacity
 
     def enqueue(self, element: int) -> None:
-        self.q.put(element)
+        with self.cond:
+            while len(self.q) >= self.capacity:
+                self.cond.wait()
+            self.q.append(element)
+            self.cond.notify_all()
+        
 
     def dequeue(self) -> int:
-        return self.q.get()
+        with self.cond:
+            while len(self.q) == 0:
+                self.cond.wait()
+            element = self.q.popleft()
+            self.cond.notify_all()
+            return element
 
     def size(self) -> int:
-        return self.q.qsize()
+        with self.cond:
+            return len(self.q)
