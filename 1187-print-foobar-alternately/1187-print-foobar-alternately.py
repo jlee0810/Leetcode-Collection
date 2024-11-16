cab7dcd98ba4004a07c6b0d98d3f4f0f1a24fc30
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-11-16 13:08:26 -0800

    Time: 79 ms (20.81%), Space: 17 MB (21.8%) - LeetHub

--- 1187-print-foobar-alternately/1187-print-foobar-alternately.py
+++ 1187-print-foobar-alternately/1187-print-foobar-alternately.py
@@ -1,20 +1,23 @@
+import threading
+
 class FooBar:
     def __init__(self, n):
         self.n = n
         self.foo_e = threading.Event()
         self.bar_e = threading.Event()
-        self.foo_e.set()  # Allow `foo` to start first
+
+        self.foo_e.set()
 
     def foo(self, printFoo: 'Callable[[], None]') -> None:
         for i in range(self.n):
-            self.foo_e.wait()  # Wait until it's `foo`'s turn
-            printFoo()  # Print "foo"
-            self.foo_e.clear()  # Reset `foo` event
-            self.bar_e.set()  # Signal `bar` to proceed
+            self.foo_e.wait()
+            printFoo()
+            self.foo_e.clear()
+            self.bar_e.set()
 
     def bar(self, printBar: 'Callable[[], None]') -> None:
         for i in range(self.n):
-            self.bar_e.wait()  # Wait until it's `bar`'s turn
-            printBar()  # Print "bar"
-            self.bar_e.clear()  # Reset `bar` event
-            self.foo_e.set()  # Signal `foo` to proceed
+            self.bar_e.wait()
+            printBar()
+            self.bar_e.clear()
+            self.foo_e.set()
