a68c5aeebf3637d130a1bd27216a9eae95f8c286
Joonwon (Eric) Lee <joonwontech@gmail.com>
2024-11-17 19:52:01 -0800

    Time: 8 ms (47.25%), Space: 16.9 MB (31.47%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -3,25 +3,22 @@ class Solution:
         cache = {}
 
         def dp(i, j):
-            if (i, j) in cache:
-                return cache[(i, j)]
-
             if i >= len(s) and j >= len(p):
                 return True
-
+            
             if j >= len(p):
                 return False
 
-            current_char_match = i < len(s) and j < len(p) and (s[i] == p[j] or p[j] == ".")
-
-            # * regex mathching
-            if j + 1 < len(p) and p[j + 1] == '*':
-                # matches zero + more of the preceding element
-                cache[(i, j)] = dp(i, j + 2) or (current_char_match and dp(i + 1, j))
+            if (i, j) in cache:
                 return cache[(i, j)]
-                
-            # no special character
-            if current_char_match:
+
+            match = i < len(s) and j < len(p) and (s[i] == p[j] or p[j] == ".")
+
+            if j + 1 < len(p) and p[j + 1] == '*':
+                cache[(i, j)] = dp(i, j + 2) or (match and dp(i + 1, j))
+                return cache[(i, j)]
+            
+            if match:
                 cache[(i, j)] = dp(i + 1, j + 1)
                 return cache[(i, j)]
             
