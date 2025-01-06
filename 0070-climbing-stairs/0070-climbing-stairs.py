fa1d157c5f4110dfc3b72dd5775458ed1c666049
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-01-06 14:05:39 +0900

    Time: 0 ms (100%), Space: 17.8 MB (9.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = {}
+        cache = {0: 1, 1: 1, 2: 2}
 
         def dp(i):
             if i in cache:
                 return cache[i]
-            if i == 1:
-                return 1
-            if i == 2:
-                return 2
             cache[i] = dp(i - 1) + dp(i - 2)
             return cache[i]
 
