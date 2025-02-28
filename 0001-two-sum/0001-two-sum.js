82a670e91f3c3c06097c2bad39c345dfc9babffe
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-02-28 10:20:16 -0800

    Time: 1 ms (89.88%), Space: 54.2 MB (14.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -9,7 +9,7 @@ var twoSum = function(nums, target) {
     for (let idx = 0; idx < nums.length; idx++) {
         var num = nums[idx];
         var other = target - num;
-        if (remainder.hasOwnProperty(other)) {
+        if (other in remainder) {
             return [remainder[other], idx];
         } 
         remainder[num] = idx;
