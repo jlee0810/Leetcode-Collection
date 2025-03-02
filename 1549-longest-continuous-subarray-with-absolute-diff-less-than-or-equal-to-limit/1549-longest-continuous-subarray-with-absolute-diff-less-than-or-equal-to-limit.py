5632666d48d3d465d3eae5ade5fee7a4872d3ffc
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-03-01 23:24:00 -0800

    Time: 489 ms (27.93%), Space: 39.8 MB (10.62%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,31 +1,22 @@
 class Solution:
-    def longestSubarray(self, nums: List[int], limit: int) -> int:
-        max_deque = deque()
-        min_deque = deque()
+    def longestSubarray(self, nums, limit):
+        max_heap = []
+        min_heap = []
+
         left = 0
         max_length = 0
 
         for right in range(len(nums)):
-            # Maintain the max_deque in decreasing order
-            while max_deque and max_deque[-1] < nums[right]:
-                max_deque.pop()
-            max_deque.append(nums[right])
+            heapq.heappush(max_heap, (-nums[right], right))
+            heapq.heappush(min_heap, (nums[right], right))
 
-            # Maintain the min_deque in increasing order
-            while min_deque and min_deque[-1] > nums[right]:
-                min_deque.pop()
-            min_deque.append(nums[right])
+            while -max_heap[0][0] - min_heap[0][0] > limit:
+                left = min(max_heap[0][1], min_heap[0][1]) + 1
 
-
-            # Check if the current window exceeds the limit
-            while max_deque[0] - min_deque[0] > limit:
-                # Remove the elements that are out of the current window
-                if max_deque[0] == nums[left]:
-                    max_deque.popleft()
-                if min_deque[0] == nums[left]:
-                    min_deque.popleft()
-                left += 1
-
+                while max_heap[0][1] < left:
+                    heapq.heappop(max_heap)
+                while min_heap[0][1] < left:
+                    heapq.heappop(min_heap)
             max_length = max(max_length, right - left + 1)
 
         return max_length
