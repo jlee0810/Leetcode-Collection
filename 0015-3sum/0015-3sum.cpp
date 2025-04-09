f54347d5dbd0444dd5ee4beba6e1f0f2b375291f
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-04-10 08:54:06 +0900

    Time: 45 ms (78.58%), Space: 29.1 MB (50.98%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
 
         vector<vector<int>> result;
 
-        int start = 0 ;
+        int start = 0;
 
         while (start < nums.size() - 2) {
             int l = start + 1;
@@ -14,13 +14,12 @@ public:
             while (l < r) {
                 int curr_sum = nums[start] + nums[l] + nums[r];
 
-                if (curr_sum == 0){
+                if (curr_sum == 0) {
                     result.push_back({nums[start], nums[l], nums[r]});
                     while (l < nums.size() - 1) {
                         if (nums[l + 1] == nums[l]) {
                             l += 1;
-                        }
-                        else {
+                        } else {
                             break;
                         }
                     }
@@ -29,24 +28,21 @@ public:
                     while (r > start + 1) {
                         if (nums[r - 1] == nums[r]) {
                             r -= 1;
-                        }
-                        else {
+                        } else {
                             break;
                         }
                     }
                     r--;
-                }
-                else if (curr_sum < 0) {
-                    l += 1;
-                }
-                else {
-                    r -= 1;
+                } else if (curr_sum < 0) {
+                    l++;
+                } else {
+                    r--;
                 }
             }
 
             while (start < nums.size() - 2) {
-                if (nums[start] == nums[start + 1]) {
-                    start += 1;
+                if (nums[start + 1] == nums[start]) {
+                    start++;
                 }
                 else {
                     break;
