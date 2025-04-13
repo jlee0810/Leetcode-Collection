182ee8e8ec63de9b6d2ee4b114a1d9132a4db6ce
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-04-13 20:05:16 +0900

    Time: 20 ms (65.96%), Space: 17.8 MB (97.97%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         max_area = 0
-        
+
         def bfs(r, c):
             nonlocal max_area
-            q = deque([(r, c)])
+            q = deque()
+            q.append((r, c))
             area = 0
             grid[r][c] = 0
 
@@ -12,15 +13,16 @@ class Solution:
                 curr_r, curr_c = q.popleft()
                 area += 1
                 for dr, dc in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-                    new_r, new_c = curr_r + dr, curr_c + dc
-                    if 0 <= new_r < len(grid) and 0 <= new_c < len(grid[0]) and grid[new_r][new_c] == 1:
-                        q.append((new_r, new_c))
-                        grid[new_r][new_c] = 0
+                    nr, nc = curr_r + dr, curr_c + dc
+                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] != 0:
+                        q.append((nr, nc))
+                        grid[nr][nc] = 0
+
             max_area = max(max_area, area)
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
                     bfs(i, j)
-
+        
         return max_area
