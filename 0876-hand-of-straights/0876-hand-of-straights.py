bfc70471f9d439c40de4ad5c5a7ab33c0be48f82
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-04-17 08:29:58 +0900

    Time: 27 ms (93.72%), Space: 19.2 MB (45.76%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -3,21 +3,22 @@ class Solution:
         if len(hand) % groupSize != 0:
             return False
 
-        cnt = Counter(hand)
-        min_heap = list(cnt.keys())
+        count = Counter(hand)
+        min_heap = list(count.keys())
         heapify(min_heap)
 
         while min_heap:
             top = min_heap[0]
+
             for i in range(groupSize):
                 curr = top + i
-                if curr not in cnt:
+                if curr not in count:
                     return False
-                cnt[curr] -= 1
-                if cnt[curr] == 0:
+                count[curr] -= 1
+
+                if count[curr] == 0:
                     if min_heap[0] != curr:
                         return False
                     else:
                         heappop(min_heap)
         return True
-
