9591e780e5a2962caa97c1c133ecc691f3e4d012
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-04-22 10:11:42 +0900

    Time: 6246 ms (12.42%), Space: 17.8 MB (87.26%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,26 +1,33 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(r, c, idx, visited):
-            if idx == len(word):
+        ROWS, COLS = len(board), len(board[0])
+        visited = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
                 return True
-            if (r, c) in visited or r < 0 or r >= len(board) or c < 0 or c >= len(board[0]) or board[r][c] != word[idx]:
+
+            if (
+                min(r, c) < 0
+                or r >= ROWS
+                or c >= COLS
+                or word[i] != board[r][c]
+                or (r, c) in visited
+            ):
                 return False
-            
+
             visited.add((r, c))
-            for dr, dc in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                nr, nc = r + dr, c + dc
-                if dfs(nr, nc, idx + 1, visited):
-                    return True
+            res = (
+                dfs(r + 1, c, i + 1)
+                or dfs(r - 1, c, i + 1)
+                or dfs(r, c + 1, i + 1)
+                or dfs(r, c - 1, i + 1)
+            )
             visited.remove((r, c))
-            return False
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    visited = set()
-                    if dfs(i, j, 0, visited):
-                        return True
+            return res
 
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
         return False
-
-
