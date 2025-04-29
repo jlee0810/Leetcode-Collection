3f7199ecdb059b77420d50f8e926f1294a853079
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-04-29 20:13:17 +0900

    Time: 22 ms (23.95%), Space: 20.3 MB (53.11%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,25 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class HeapNode:
+    def __init__(self, node):
+        self.node = node
+
+    def __lt__(self, other):
+        return self.node.val < other.node.val
+
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        min_heap = []
-        dummy = ListNode()
-        cnt = 0
+        minHeap = []
 
         for lst in lists:
             if lst:
-                heappush(min_heap, [lst.val, cnt, lst])
-            cnt += 1
+                heappush(minHeap, HeapNode(lst))
+
+        dummy = ListNode()
 
         curr = dummy
-        while min_heap:
-            curr_val, _, curr_lst = heappop(min_heap)
-            curr.next = curr_lst
+
+        while minHeap:
+            currNode = heappop(minHeap)
+            currNode = currNode.node
+
+            if currNode.next:
+                heappush(minHeap, HeapNode(currNode.next))
+            curr.next = currNode
             curr = curr.next
-            
-            if curr_lst.next:
-                heappush(min_heap, [curr_lst.next.val, cnt, curr_lst.next])
-                cnt += 1
-            
+
         return dummy.next
