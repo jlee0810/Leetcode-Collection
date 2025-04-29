bcf74e70e0e6b271dc7927ec1a806c6d50de0439
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-04-29 10:49:22 +0900

    Time: 127 ms (71.58%), Space: 78.6 MB (46.01%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,57 +1,56 @@
 class Node:
     def __init__(self):
+        self.key = None
         self.val = None
         self.next = None
         self.prev = None
-        self.key = None
+
 
 class LRUCache:
-
-    def __init__(self, capacity: int):
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.map = {}
+        self.size = 0
         self.lru = Node()
         self.recent = Node()
         self.lru.next = self.recent
         self.recent.prev = self.lru
-        self.size = 0
-
-    def remove(self, key):
-        node = self.map[key]
-        del self.map[key]
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        self.size -= 1
+        self.map = {}
 
     def add(self, key, val):
-        new_node = Node()
-        new_node.val = val
-        new_node.key = key
-        self.map[key] = new_node
-        self.recent.prev.next = new_node
-        new_node.prev = self.recent.prev
-        new_node.next = self.recent
-        self.recent.prev = new_node
+        new = Node()
+        new.key = key
+        new.val = val
+        self.map[key] = new
+        self.recent.prev.next = new
+        new.prev = self.recent.prev
+        new.next = self.recent
+        self.recent.prev = new
         self.size += 1
 
-    def get(self, key: int) -> int:
-        if key in self.map:
-            val = self.map[key].val
-            self.remove(key)
-            self.add(key, val)
-            return val
-        return -1
+    def delete(self, key):
+        old = self.map[key]
+        del self.map[key]
+        self.size -= 1
 
+        old.prev.next = old.next
+        old.next.prev = old.prev
+
+    def get(self, key):
+        if key in self.map:
+            node = self.map[key]
+            self.delete(key)
+            self.add(key, node.val)
+            return node.val
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
-            self.remove(key)
+            self.delete(key)
             self.add(key, value)
         else:
-            if self.size == self.capacity:
-                self.remove(self.lru.next.key)
+            if self.size >= self.capacity:
+                self.delete(self.lru.next.key)
             self.add(key, value)
-
 
 
 # Your LRUCache object will be instantiated and called as such:
