e408b80398906c7bcfc15c848cb2ac4d96355d93
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-05-01 09:16:22 +0900

    Time: 3 ms (16.75%), Space: 17.8 MB (44.5%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -1,21 +1,54 @@
-from fractions import Fraction
-
 class Solution:
     def fractionAddition(self, expression: str) -> str:
-        fractions = []
-        num = ""
-        if expression[0] != '-':
-            expression = '+' + expression
+        num = 0
+        denom = 1
+        idx = 0
 
-        for i, c in enumerate(expression):
-            if c in ['+', '-']:
-                if num:
-                    fractions.append(Fraction(num))
-                num = c
+        while idx < len(expression):
+            curr_num = 0
+            curr_denom = 0
+
+            is_neg = False
+
+            if expression[idx] == "-" or expression[idx] == "+":
+                if expression[idx] == "-":
+                    is_neg = True
+                idx += 1
+
+            while idx < len(expression) and expression[idx].isdigit():
+                curr_num = curr_num * 10 + int(expression[idx])
+                idx += 1
+
+            if is_neg:
+                curr_num *= -1
+
+            idx += 1
+
+            while idx < len(expression) and expression[idx].isdigit():
+                curr_denom = curr_denom * 10 + int(expression[idx])
+                idx += 1
+
+            num = num * curr_denom + curr_num * denom
+            denom = denom * curr_denom
+
+            if num == 0:
+                denom = 1
             else:
-                num += c
-        fractions.append(Fraction(num))
+                gcd = self._find_gcd(abs(num), denom)
+                num //= gcd
+                denom //= gcd
+
+        return f"{num}/{denom}"
+
+    def _find_gcd(self, a, b):
+        while b:
+            a, b = b, a % b
         
-        result = sum(fractions)
-        
-        return f"{result.numerator}/{result.denominator}"
+        return a
+
+
+        # min_num = min(a, b)
+        # for i in range(min_num, 0, -1):
+        #     if a % i == 0 and b % i == 0:
+        #         return i
+        # return 1
