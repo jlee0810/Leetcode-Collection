53d050078da5337da71708e1bcfeba204f549f6c
Joonwon (Eric) Lee <joonwontech@gmail.com>
2025-05-11 17:42:33 +0900

    Time: 2 ms (33.2%), Space: 18 MB (16.58%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -3,16 +3,16 @@ class Solution:
         if not s or s[0] == "0":
             return 0
 
-        n = len(s)
-        dp = [0] * n
+        if not s or s[0] == "0":
+            return 0
+
+        dp = [0 for _ in range(len(s))]
         dp[0] = 1
 
-        for i in range(1, n):
+        for i in range(1, len(s)):
             if s[i - 1] != "0":
-                two_digit = s[i - 1 : i + 1]
-                if 1 <= int(two_digit) <= 26:
+                if 1 <= int(s[i - 1 : i + 1]) <= 26:
                     dp[i] += dp[i - 2] if i >= 2 else 1
-
             if s[i] != "0":
                 dp[i] += dp[i - 1]
 
